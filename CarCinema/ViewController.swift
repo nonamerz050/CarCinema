6f8644d0655853e53708fd032d83084d870f9cef
Nonamerz <a.g.kolesnik@yandex.ru>
2020-11-13 21:13:03 +0300

    Initial Commit

---
+++ CarCinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarCinema
+//
+//  Created by MacBook Pro on 13.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
